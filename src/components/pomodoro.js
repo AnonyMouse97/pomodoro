8a5bd4b5ff16f1cefb0ea19fb07e14e97cfe181a
letocart samuel <samuel.le.belgique@gmail.com>
2021-04-30 00:02:59 +0200

    end day fixes

--- src/components/pomodoro.js
+++ src/components/pomodoro.js
@@ -1,30 +1,30 @@
-import React, {useState} from "react";
-import {SESSION_DURATION} from "../core/constants";
+import React, { useState, useCallback } from "react";
+import { SESSION_DURATION } from "../core/constants";
 
 import Display from "./display/display";
 import Tools from "./tools/tools";
 
 const Pomodoro = () => {
-    const [seconds, setSeconds] = useState(SESSION_DURATION);
-    const [running] = useState(false);
+  const [seconds, setSeconds] = useState(SESSION_DURATION);
+  const [running] = useState(false);
 
-    const handleMinus = () => setSeconds(Math.max(seconds - 60, 60));
-    const handlePlus = () => setSeconds(seconds + 60);
-    const handleStartPause = () => console.warn("handleReset");
-    const handleReset = () => setSeconds(SESSION_DURATION);
+  const handleMinus = useCallback(() => setSeconds(Math.max(seconds - 60, 60)), [seconds, setSeconds]);
+  const handlePlus = useCallback(() => setSeconds(seconds + 60), [seconds, setSeconds]);
+  const handleStartPause = () => console.warn("handleReset");
+  const handleReset = useCallback(() => setSeconds(SESSION_DURATION), [setSeconds]);
 
-    return (
-        <div>
-            <Display seconds={seconds} running={running} />
-            <Tools
-                running={running}
-                onMinus={handleMinus}
-                onPlus={handlePlus}
-                onStartPause={handleStartPause}
-                onReset={handleReset}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Display seconds={seconds} running={running} />
+      <Tools
+        running={running}
+        onMinus={handleMinus}
+        onPlus={handlePlus}
+        onStartPause={handleStartPause}
+        onReset={handleReset}
+      />
+    </div>
+  );
 };
 
 export default Pomodoro;
